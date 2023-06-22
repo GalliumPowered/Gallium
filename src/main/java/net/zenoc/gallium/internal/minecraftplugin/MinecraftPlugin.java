e85b54b0cdfb9f22798a74560b645871877c055c
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-06-22 17:10:55 +1200

    JSON & Annotation plugin implementation

--- src/main/java/net/zenoc/gallium/internal/minecraftplugin/MinecraftPlugin.java
+++ src/main/java/net/zenoc/gallium/internal/minecraftplugin/MinecraftPlugin.java
@@ -1,15 +1,12 @@
-package net.zenoc.gallium.internal.minecraftplugin;
-
-import net.zenoc.gallium.plugin.JavaPlugin;
-import net.zenoc.gallium.api.annotations.Plugin;
-
-@Plugin(
-        name = "Minecraft",
-        id = "minecraft",
-        description = "Minecraft",
-        authors = {"Mojang"},
-        version = "1.17.1"
-)
-public class MinecraftPlugin extends JavaPlugin {
-    // TODO: Assign Minecraft default commands to this
-}
+//package net.zenoc.gallium.internal.minecraftplugin;
+//
+//@Plugin(
+//        name = "Minecraft",
+//        id = "minecraft",
+//        description = "Minecraft",
+//        authors = {"Mojang"},
+//        version = "1.17.1"
+//)
+//public class MinecraftPlugin extends JavaPlugin {
+//    // TODO: Assign Minecraft default commands to this
+//}
