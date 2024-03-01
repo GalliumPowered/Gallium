ab1850bae749862ef5c6f624b9f514d22c19efd0
Findlay Richardson <findlayrichardson3@gmail.com>
2024-03-01 20:37:57 +1300

    Death event

---
+++ testplugin/src/main/java/org/galliumpowered/testplugin/listeners/PlayerDeathListener.java
@@ -0,0 +1,16 @@
+package org.galliumpowered.testplugin.listeners;
+
+import net.kyori.adventure.text.Component;
+import org.galliumpowered.Gallium;
+import org.galliumpowered.annotation.EventListener;
+import org.galliumpowered.chat.Colors;
+import org.galliumpowered.event.player.PlayerDeathEvent;
+
+public class PlayerDeathListener {
+    @EventListener
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        if (!event.isCancelled()) {
+            Gallium.getServer().sendMsgToAll(Component.text(Colors.GREEN + event.getPlayer().getName() + " " + event.getDeathCause()));
+        }
+    }
+}
